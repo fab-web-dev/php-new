54196b86c131e9017785ef072c2277ce707fda87
fab-web-dev <97179540+fab-web-dev@users.noreply.github.com>
2022-03-21 16:54:43 +0100

    2nd commit

--- clean-blog/model/post1.model.php
+++ clean-blog/model/post1.model.php
@@ -1,8 +1,53 @@
 <?php
-$title = "Cyril Lignac";
-$subtitle = "Biographie";
-$content = "Le plaisir, la gourmandise, le partage sont autant de repères pour suivre le chemin emprunté par Cyril Lignac depuis ses débuts. Son histoire est celle d’une passion, née en Aveyron et cultivée de ses CAP de pâtissier-glacier-chocolatier et cuisinier, jusqu’au soin apporté à l’élaboration de chaque nouveau projet.
+if ( (!isset($_GET['article'])) || ($_GET['article'] == '1') ) {
 
-En 2005, repéré par une productrice de télévision, il se lance dans l’aventure « Oui, Chef ! » jusqu’à l’ouverture de son premier restaurant, Le Quinzième. Celui-ci sera récompensé 7 ans plus tard d’une étoile au Guide Michelin et fermera ses portes après 15 ans d’exercice pour faire place à une nouvelle aventure. Ce programme sera le premier des nombreux succès médiatiques tels « Le Meilleur Pâtissier » ou « Tous en cuisine », faisant de lui le chef préféré des français.
+    $bg = 'assets/img/post-bg.jpg';
+    $color = "green";
+    $title = 'Planche de surf';
+    $subtitle = 'Comment bien la choisir ?';
+    $content = 'Vous commencez à prendre vos premières <b>vagues</b> et souhaitez passer le cap dans la pratique de votre sport préféré ? Sans doute le moment est-il venu de vous offrir votre première planche de surf ! Petit bémol : il existe presqu’autant de modèle que de shapers… Aussi, choisir sa planche de surf peut s’avérer un peu plus compliqué qu’on ne le croit au premier abord.
 
-En 2008, Cyril Lignac révèle ses talents de restaurateur-entrepreneur. Il reprend Le Chardenoux, une institution du Paris 11e, puis s’installe en plein cœur de Saint-Germain-des-Prés avec Aux Prés, reprise d’un célèbre bistrot parisien, Le Bar des Prés, aux façons d’ici et d’Asie, et le bar à cocktails couture Dragon. En lieu et place de l’étoilé Quinzième, Ischia livre sa lecture personnelle et authentique de la si riche culture gastronomique italienne. 2021 voit l’ouverture de son premier restaurant à l’international avec le Bar des Prés London, installé au cœur de Mayfair.";
+    Quel shape est fait pour vous ? Quel type de vagues voulez-vous surfer ? Comment va-t-elle se comporter à la rame ? Quid du poids et de la <a href="#">taille</a> ?
+
+    Tous ces éléments – vous l’aurez devinez – et bien d’autres encore, vont être déterminant pour votre montée en niveau. Dans cet article, on vous livre tous les secrets pour choisir la planche de surf en adéquation avec votre niveau et vos objectifs !
+
+    <style>
+        .test {
+            color: ' . $color . ';
+        }
+    </style>
+
+    1. Pourquoi choisir sa planche de surf ?
+    2. Quels sont les différents types de planche de surf ? 
+    3. Comment choisir sa planche de surf ?
+    4. Où acheter sa <span class="test">planche de surf</span> ?';
+
+}
+
+else if ($_GET['article'] == '2') {
+
+$bg = 'https://www.surfcamp.fr/wp-content/uploads/2020/03/image-67.jpg';
+$color = "";
+$title = 'Découvrez notre sélection';
+$subtitle = 'Les dix meilleurs spots de surf en rance';
+$content = 'La mythique plage d’Hossegor se situe dans les Landes. Elle est surnommée par certains le  Hawaï  de l’Europe. Pour y accéder, prenez le TGV à la gare de Paris Montparnasse et descendez à Bayonne quatre heures plus tard';
+
+}
+
+else if ($_GET['article'] == '3') {
+
+    $bg = 'https://www.surfcamp.fr/wp-content/uploads/2020/03/image-6-1.jpg';
+    $color = "";
+    $title = 'Nazare surf : les différents spots';
+    $subtitle = 'Praia do Norte';
+    $content = 'Le spot de surf le plus connu de Nazaré est Praia do Norte, avec les plus grandes vagues du Portugal, voire du monde. Ce surf est destiné aux surfeurs vraiment experts. Les vagues à Praia do Norte sont grandes, lourdes et puissantes. Même si vous ne savez pas surfer, il peut valoir la peine de venir à Nazaré simplement pour observer les performances des surfeurs Big wave. Les meilleures conditions à Praia da Norte sont celles où le vent vient de l’est pour un swell nord-ouest. C’est un beach break qui fonctionne à marée basse.';
+    
+}
+
+else {
+
+    $bg = 'https://img.freepik.com/vecteurs-libre/glitch-error-404-page_23-2148105404.jpg';
+    $title = 'Erreur 404';
+    $subtitle = 'Page inexistante !';
+    $content = '';
+}
--- clean-blog/post.php
+++ clean-blog/post.php
@@ -1,18 +1,11 @@
-<?php include("model/post1.model.php") ?>
+<?php
+// var_dump($_GET);
+// exit;
+// die($_GET['article']);
+// echo($_GET['article']);
 
-<?php include("nav.inc.php"); ?>
-<!-- Page Header-->
-<?php include("header.inc.php"); ?>
-    <!-- Post Content-->
-    <article class="mb-4">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <p>
-                        <?php echo $content; ?>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </article>
-        <?php include("footer.inc.php"); ?>
+include("model/post1.model.php"); 
+
+$layout_title = 'Les articles';
+        
+include("view/post.view.php");
